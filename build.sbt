b8b98e599756e8c6bceeadb91b336b09ec25d4a0
Vadim Osmolovski <osvadimos@gmail.com>
2018-03-17 17:49:45 +0300

    Installed tensorflow dependency

--- build.sbt
+++ build.sbt
@@ -5,4 +5,4 @@ version := "0.1"
 scalaVersion := "2.12.4"
 
 resolvers += Resolver.sonatypeRepo("snapshots")
-libraryDependencies += "org.platanios" %% "tensorflow" % "0.1.0-SNAPSHOT"
+libraryDependencies += "org.platanios" %% "tensorflow" % "0.1.0"classifier "darwin-cpu-x86_64"
--- src/main/scala/com/datapipesoft/tensor/MainTensor.scala
+++ src/main/scala/com/datapipesoft/tensor/MainTensor.scala
@@ -1,9 +1,15 @@
 package com.datapipesoft.tensor
 
+import org.platanios.tensorflow.api.core.Shape
+import org.platanios.tensorflow.api.tensors.Tensor
+import org.platanios.tensorflow.api.types.INT32
+
 object MainTensor {
 
   def main(args: Array[String]): Unit = {
-    println("TEst")
+    println("Test")
+    val tensor = Tensor.zeros(INT32, Shape(2, 5))
+    println(tensor.toString())
   }
 
 }
